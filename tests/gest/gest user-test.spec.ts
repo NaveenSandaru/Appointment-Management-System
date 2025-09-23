345fda784a09c00c55120fb31c64cf8b6604ba8c
RW Sandaru Piumantha <132807012+RWSandaru8@users.noreply.github.com>
2025-09-23 09:09:56 +0530

    123

--- tests/gest/gest user-test.spec.ts
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('gest user flow', async ({ page }) => {
-  await page.goto('http://localhost:3000/');
-  await page.getByRole('button', { name: 'Browse Service Providers' }).first().click();
-  await page.getByRole('button', { name: 'Book Now' }).first().click();
-});
